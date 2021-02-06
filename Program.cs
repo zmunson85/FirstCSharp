fbfd71993464800c6e16a4dfe83a999c817be398
Zachary Munson <zmunson85@gmail.com>
2021-02-06 02:22:24 -0700

    first c# repo

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace test_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname,lname;
+            Console.Write("Enter Your First Name : ");
+            fname =Console.ReadLine();
+            Console.Write("Enter Your Last Name : ");
+            lname = Console.ReadLine();
+            Console.Write("Your Full Name Is "+fname+","+lname);
+        }
+    }
+}
